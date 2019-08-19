a7b1abd0a72a8624603f42e403dd9b601f4a24b0
Shashi Kiran <kiranshashiny@gmail.com>
2019-08-19 15:11:05 +0530

    Updated

--- 4_temp_hmdty_airp_station/4_temp_hmdty_airp_station.ino
+++ 4_temp_hmdty_airp_station/4_temp_hmdty_airp_station.ino
@@ -5,6 +5,8 @@
 
 #include <ESP8266WiFi.h>
 #include<dht.h>
+#include <SFE_BMP180.h> //Including BMP180 library
+
 dht DHT;
 #define DHT11_PIN D7
 
@@ -12,7 +14,11 @@ dht DHT;
 char ssid[] = "Wemos_AP";           // SSID of your AP
 char pass[] = "Wemos_comm";         // password of your AP
 char msg[50];
+char newmsg[50];
 
+#define ALTITUDE 920.00 //Altitude where I live (change this to your altitude)
+
+SFE_BMP180 pressure; //Creating an object
 
 IPAddress server(192,168,4,15);     // IP address of the AP
 WiFiClient client;
@@ -34,9 +40,19 @@ void setup() {
   Serial.println("MAC:" + WiFi.macAddress());
   Serial.print("Gateway:"); Serial.println(WiFi.gatewayIP());
   Serial.print("AP MAC:"); Serial.println(WiFi.BSSIDstr());
+
+  // Init the Pressure sensor
+  if (pressure.begin()) //If initialization was successful, continue
+    Serial.println("BMP180 init success");
+  else {
+    Serial.println("BMP180 init failed !!!!");
+  }
 }
 
 void loop() {
+  double T, P, p0;
+  char status;
+  
   int chk = DHT.read11(DHT11_PIN); 
 
   Serial.print("Temperature:");
@@ -48,34 +64,64 @@ void loop() {
   
   client.connect(server, 80);
   Serial.println("********************************");
-  Serial.println("Byte sent to the AP: ");
-
+  
   // Temperature
   float t = DHT.temperature;
   snprintf (msg, 75, "tempr, %0.2f C\r", t);
-
-  Serial.print(">>Publish message: ");
-  Serial.println(msg);
   client.print(msg);
+  Serial.println(msg);
+  //client.flush();
   
-  String answer = client.readStringUntil('\r');
-  Serial.println("From the AP: " + answer);
+  //String answer = client.readStringUntil('\r');
+  //Serial.println("From the AP: " + answer);
   client.flush();
-  delay(1000);
-  //Humidity
+  client.stop();
+  delay(2000);
+  
+  // To send the second sensor data....
+  // Strange !  I have to connect(server..) again to send data.
+  // or else it wont go.
+
+  //Humidity  
+  client.connect(server, 80);  
   float h = DHT.humidity;
-  snprintf (msg, 75, "hmdty, %0.2f \%% \r", h);
-
-  Serial.print(">>Publish message: ");
-  Serial.println(msg);
-  client.print(msg);
-  
-  answer = client.readStringUntil('\r');
-  Serial.println("<<From the AP: " + answer);
+  sprintf (newmsg, "hmdty, %0.2f \%%\r", h);
+  client.print(newmsg);
+  Serial.println (newmsg);
   client.flush();
-  
   client.stop();
   delay(3000);
 
-}
+  
+  // Air Pressure
+  //Serial.print("You provided altitude: ");
+  //Serial.print(ALTITUDE, 0);
+  //Serial.println(" meters");
 
+  status = pressure.startPressure(3);
+
+  if (status != 0) {
+      delay(status);
+
+      status = pressure.getPressure(P, T);
+      if (status != 0) {
+          //Serial.print("Pressure measurement: ");
+          //Serial.print(P);
+          //Serial.println(" hPa (Pressure measured using temperature)");
+
+          p0 = pressure.sealevel(P, ALTITUDE);
+          //Serial.print("Relative (sea-level) pressure: ");
+          //Serial.print(p0);
+          //Serial.println("hPa");
+          
+          client.connect(server, 80);
+          long ap = (long) p0;  
+          sprintf (newmsg, "airp, %ld hPa\r", ap);
+          client.print(newmsg);
+          Serial.println (newmsg);
+          client.flush();
+          client.stop();
+          delay(3000);
+      }
+  }
+}
